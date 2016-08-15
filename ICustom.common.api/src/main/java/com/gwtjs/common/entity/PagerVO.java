405b1e7fb4df8223c7203da3a2c0a1147e75e556
zgdong <flash8627@hotmail.com>
2016-08-16 00:08:53 +0800

    page common and lookup search develop

--- ICustom.common.api/src/main/java/com/gwtjs/common/entity/PagerVO.java
+++ ICustom.common.api/src/main/java/com/gwtjs/common/entity/PagerVO.java
@@ -1,6 +1,5 @@
 package com.gwtjs.common.entity;
 
-import java.util.Collections;
 
 /**
  * 分页组件
@@ -12,15 +11,37 @@ public class PagerVO {
 
 	private Object entityList;
 	private int totalRows;
-	private int curPage;
-	private int pageSize;
-	private int startRecord;
-	private int endRecord;
+	private int curPage = 1;
+	private int pageSize = 20;
+	/** 配合结果模式为0取结果集与记录总，为1取记录数，为2只取结果集 */
+	private int resultMode;
+	private int startIndex;
+	private int endIndex;
 	private int totalPage;
-	private String order;
+	/* 分页表达式 */
+	private String orderBy;
+
+	public int getStartIndex() {
+		return startIndex == 0?(curPage-1)*pageSize+1:startIndex;
+	}
+
+	public void setStartIndex(int startIndex) {
+		this.startIndex = startIndex;
+	}
+
+	public int getEndIndex() {
+		if(endIndex-startIndex>pageSize){
+			return startIndex+pageSize;
+		}
+		return endIndex == 0? curPage*pageSize:(endIndex-1>totalRows?totalRows:endIndex-1);
+	}
+
+	public void setEndIndex(int endIndex) {
+		this.endIndex = endIndex;
+	}
 
 	public Object getEntityList() {
-		return entityList == null ? Collections.<Object> emptyList() : entityList;
+		return entityList;
 	}
 
 	public void setEntityList(Object entityList) {
@@ -51,6 +72,14 @@ public class PagerVO {
 		this.pageSize = pageSize;
 	}
 
+	public int getResultMode() {
+		return resultMode;
+	}
+
+	public void setResultMode(int resultMode) {
+		this.resultMode = resultMode;
+	}
+
 	public int getTotalPage() {
 		return totalPage;
 	}
@@ -59,28 +88,12 @@ public class PagerVO {
 		this.totalPage = totalPage;
 	}
 
-	public String getOrder() {
-		return order;
+	public String getOrderBy() {
+		return orderBy;
 	}
 
-	public void setOrder(String order) {
-		this.order = order;
+	public void setOrderBy(String orderBy) {
+		this.orderBy = orderBy;
 	}
 
-	public int getStartRecord() {
-		return curPage*pageSize-1;
-	}
-
-	public void setStartRecord(int startRecord) {
-		this.startRecord = startRecord;
-	}
-
-	public int getEndRecord() {
-		return curPage*pageSize+pageSize;
-	}
-
-	public void setEndRecord(int endRecord) {
-		this.endRecord = endRecord;
-	}
-	
 }
