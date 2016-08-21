82a6a3ddcc44d0d64e2a2ba23b00e5ebd5cf346f
zgdong <flash8627@hotmail.com>
2016-08-21 23:17:54 +0800

    page update and database update

--- ICustom documents/db script/update.sql
+++ ICustom documents/db script/update.sql
@@ -55,14 +55,16 @@ create table SYS_REGISTER_T
   reg_name         VARCHAR2(60) not null,
   reg_code         VARCHAR2(60),
   reg_desc         VARCHAR2(200),
+  reg_value        VARCHAR2(200),
+  parent_path      VARCHAR2(200),
   parent_id        NUMBER(30),
-  comments		VARCHAR2(308),
   order_code  INTEGER default 1,
   created_user NUMBER,
   created_date DATE default SYSDATE,
   update_last_user NUMBER,
   update_last_date DATE default SYSDATE,
-  valid_flag       NUMBER(1) default 1
+  valid_flag       NUMBER(1) default 1,
+  CONSTRAINT SYS_REGISTER_T_PPATH_UNIQUE UNIQUE (parent_path)
 );
 -- Add comments to the table 
 comment on table SYS_REGISTER_T
@@ -96,15 +98,20 @@ alter table SYS_REGISTER_T
 create table SYS_LOOKUP_ITEM_T
 (
   item_id          VARCHAR2(32) not null,
+  item_code        INTEGER,
   item_name        VARCHAR2(40),
+  item_lan         VARCHAR2(40),
   class_id         VARCHAR2(32),
+  item_status      INTEGER default 1,
   comments    VARCHAR2(308),
   order_code  INTEGER default 1,
   created_user NUMBER,
   created_date DATE default SYSDATE,
   update_last_user NUMBER,
   update_last_date DATE default SYSDATE,
-  valid_flag       NUMBER(1) default 1
+  valid_flag       NUMBER(1) default 1,
+  CONSTRAINT SYS_LOOKUP_ITEM_NAME_UNIQUE UNIQUE (item_name),
+  CONSTRAINT SYS_LOOKUP_ITEM_CODE_UNIQUE UNIQUE (item_code)
 );
 -- Add comments to the table 
 comment on table SYS_LOOKUP_ITEM_T
@@ -144,7 +151,9 @@ create table SYS_LOOKUP_CLASSIFY_T
   created_date DATE default SYSDATE,
   update_last_user NUMBER,
   update_last_date DATE default SYSDATE,
-  valid_flag       NUMBER(1) default 1
+  valid_flag       NUMBER(1) default 1,
+  CONSTRAINT SYS_LOOKUP_CLASS_NAME_UNIQUE UNIQUE (class_name),
+  CONSTRAINT SYS_LOOKUP_CLASS_CODE_UNIQUE UNIQUE (class_code)
 );
 -- Add comments to the table 
 comment on table SYS_LOOKUP_CLASSIFY_T
@@ -173,34 +182,42 @@ alter table SYS_LOOKUP_CLASSIFY_T
 -- Create table
 create table SYS_LANGUAGE_T
 (
-  lan_id      NUMBER not null PRIMARY KEY,
-  lan_code    VARCHAR2(68) not null,
-  lan_type    INTEGER default 1,
-  comments    VARCHAR2(308),
-  attribute1  VARCHAR2(100),
-  attribute2  VARCHAR2(100),
-  attribute3  VARCHAR2(100),
-  attribute4  VARCHAR2(100),
-  attribute5  VARCHAR2(100),
-  attribute6  VARCHAR2(100),
-  attribute7  VARCHAR2(100),
-  attribute8  VARCHAR2(100),
-  attribute9  VARCHAR2(100),
-  attribute10 VARCHAR2(100),
-  order_code  INTEGER default 1,
-  created_user NUMBER,
-  created_date DATE default SYSDATE,
+  lan_id           NUMBER not null,
+  lan_code         VARCHAR2(68) not null,
+  lan_value        VARCHAR2(68) not null,
+  lan_type         INTEGER default 1,
+  comments         VARCHAR2(308),
+  attribute1       VARCHAR2(100),
+  attribute2       VARCHAR2(100),
+  attribute3       VARCHAR2(100),
+  attribute4       VARCHAR2(100),
+  attribute5       VARCHAR2(100),
+  attribute6       VARCHAR2(100),
+  attribute7       VARCHAR2(100),
+  attribute8       VARCHAR2(100),
+  attribute9       VARCHAR2(100),
+  attribute10      VARCHAR2(100),
+  order_code       INTEGER default 1,
+  created_user     NUMBER,
+  created_date     DATE default SYSDATE,
   update_last_user NUMBER,
   update_last_date DATE default SYSDATE,
-  valid_flag       NUMBER(1) default 1
+  valid_flag       NUMBER(1) default 1,
+  CONSTRAINT SYS_LANGUAGE_UNIQUE UNIQUE (lan_value, lan_type)
 );
 -- Add comments to the columns 
 comment on column SYS_LANGUAGE_T.lan_id
   is '主键,序列自增';
 comment on column SYS_LANGUAGE_T.lan_code
   is '语言编码,带包的风格 icustom.menu.home,也可以做组的作用，一个code可以包括中英法';
+comment on column SYS_LANGUAGE_T.lan_value
+  is '语言对应值：如：菜单对应英文menu';
 comment on column SYS_LANGUAGE_T.lan_type
   is '语言类型,中英法';
+comment on column SYS_LANGUAGE_T.attribute1
+  is '扩展属性1';
+comment on column SYS_LANGUAGE_T.attribute2
+  is '扩展属性2';
 comment on column SYS_LANGUAGE_T.created_user
   is '创建用户';
 comment on column SYS_LANGUAGE_T.created_date
@@ -209,6 +226,10 @@ comment on column SYS_LANGUAGE_T.update_last_user
   is '最后更新用户';
 comment on column SYS_LANGUAGE_T.update_last_date
   is '最后更新日期';
+-- Create/Recreate primary, unique and foreign key constraints 
+alter table SYS_LANGUAGE_T
+  add primary key (LAN_ID)
+  using index; 
 
 -- Create table
 create table SYS_USERS_T
